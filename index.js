9393368ec47f177db7e7db3e73813a3c037a0d81
Rafael C. Sequeira <068755@unifeso.lan>
2021-12-02 09:50:44 -0300

    Ok

--- index.js
+++ index.js
@@ -44,7 +44,24 @@ function hideTextElements() {
   duplaTxt.style.display = "none"
 }
 
+var i = 1;
+var p = 1;
 $( "#btn" ).click(function() {
-  $( "#input-container" ).append('<input type="text" name="jogador" id="jogador" placeholder="Nome do jogador"> <input type="number" name="pontos" id="pontos" placeholder="Pontuação">');
-  
+  $( "#inputs" ).append('<input class="jog" type="text" name="jogador" id="jogador'+i+'" placeholder="Nome do jogador"> <input class="pont" type="number" name="pontos" id="pontos'+p+'" placeholder="Pontuação">');
+  i++;
+  p++;
+});
+
+let pistas = document.querySelector("#pistas");
+let btnJogar = document.querySelector('#btn-jogar');
+let jogadores = document.getElementsByClassName("jog");
+
+
+
+btnJogar.addEventListener('click', () => {
+  let calc = jogadores.length;
+  let pist = Number(pistas.value)
+  let div = Number(calc);
+  // console.log(pist);
+  window.navigator.vibrate(200);
 });
